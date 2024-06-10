eb5c9809439eb53720b9c58f8d088170e0a354b1
승환 <ksh7353@naver.com>
2024-06-10 19:34:14 +0900

    componentScan Test

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,10 +2,12 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
 /*
     vip라면 -> 금액별 10% 할인 적용!
  */
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountRateAmount = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,13 +6,17 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     //인터페이스에만 의존! 구체적인 클래스 의존x -> DIP 완벽!
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
